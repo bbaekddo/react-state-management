f8bc86d99bfeab73a2dea650d79e90393d47e9ea
bbaekddo <bbaekddo100@naver.com>
2025-09-29 09:35:10 +0900

    🚧 Summary 페이지 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import CompletePage from "./pages/CompletePage";
+import OrderPage from "./pages/OrderPage";
+import SummaryPage from "./pages/SummaryPage";
 
-function App() {
-	const [count, setCount] = useState(0);
-
+function App(): React.ReactElement {
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/summary" element={<SummaryPage />} />
+				<Route path="/order" element={<OrderPage />} />
+				<Route path="/complete" element={<CompletePage />} />
+				{/* 기본 경로 리다이렉션 처리 */}
+				<Route path="*" element={<Navigate to="/summary" replace />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/pages/SummaryPage/index.tsx
+++ src/pages/SummaryPage/index.tsx
@@ -1,5 +1,17 @@
+import { useId } from "react";
+
 const SummaryPage = () => {
-	return <div>Summary</div>;
+	const checkboxId = useId();
+	return (
+		<div>
+			<form action="">
+				<label htmlFor={checkboxId}>
+					I confirm that I have read the terms and conditions
+				</label>
+				<input type="checkbox" checked={false} id={checkboxId} />
+			</form>
+		</div>
+	);
 };
 
 export default SummaryPage;
