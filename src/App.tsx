bcd4f48713a615d6dd5e645351b49fa1b4395809
bbaekddo <bbaekddo100@naver.com>
2026-08-15 16:54:04 +0200

    🚧 useSelector, useDispatch 훅

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,51 @@
 import "./App.css";
 import * as React from "react";
-import {useState} from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 type AppProps = {
-	value: any;
-	onIncrement: () => void;
-	onDecrement: () => void;
-}
+  value: any;
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
 
-function App({ value, onIncrement, onDecrement }: AppProps): React.ReactElement {
-	// 상태 관리
-	const [todoValue, setTodoValue] = useState("");
+function App({
+  value,
+  onIncrement,
+  onDecrement,
+}: AppProps): React.ReactElement {
+  const counter = useSelector((state: any) => state.counter);
+  const todos: string[] = useSelector((state: any) => state.todos);
+  const dispatch = useDispatch();
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setTodoValue(e.target.value);
-	};
+  const [todoValue, setTodoValue] = useState("");
 
-	const addTodo = (e:React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		setTodoValue("");
-	}
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
 
-	return (
-		<div>
-			Clicked: {value} times
-			<button onClick={onIncrement}>
-				+
-			</button>
-			<button onClick={onDecrement}>
-				-
-			</button>
-			<form onSubmit={addTodo}>
-				<input type="text" value={todoValue} onChange={handleChange}/>
-				<button type="submit">Add</button>
-			</form>
-		</div>
-	);
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
+    setTodoValue("");
+  };
+
+  return (
+    <div>
+      Clicked: {counter} times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,36 +2,33 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
-const store = createStore(rootReducer);
-
-store.dispatch({
-	type: "ADD_TODO",
-	text: "test",
+const store = configureStore({
+  reducer: rootReducer,
 });
 
 if (rootElement) {
-	const root = createRoot(rootElement);
-	const render = () => {
-		root.render(
-			<StrictMode>
-				<Provider store={store}>
-					<App
-						value={store.getState().counter}
-						onIncrement={() => store.dispatch({ type: "increment" })}
-						onDecrement={() => store.dispatch({ type: "decrement" })}
-					/>
-				</Provider>
-			</StrictMode>,
-		);
-	};
+  const root = createRoot(rootElement);
+  const render = () => {
+    root.render(
+      <StrictMode>
+        <Provider store={store}>
+          <App
+            value={store.getState().counter}
+            onIncrement={() => store.dispatch({ type: "increment" })}
+            onDecrement={() => store.dispatch({ type: "decrement" })}
+          />
+        </Provider>
+      </StrictMode>,
+    );
+  };
 
-	store.subscribe(render);
-	render();
+  store.subscribe(render);
+  render();
 } else {
-	console.error("Root 요소를 찾을 수 없습니다.");
+  console.error("Root 요소를 찾을 수 없습니다.");
 }
