0107ce4572ca30e600cbc2de596c49f97ed31cf2
bbaekddo <bbaekddo100@naver.com>
2026-08-16 19:32:22 +0200

    🎨 Prettier 적용

--- src/reducers/counter.tsx
+++ src/reducers/counter.tsx
@@ -1,9 +1,12 @@
-const counter = (state = 0, action: {type: string }) => {
-    switch (action.type) {
-        case "increment": return state + 1;
-        case "decrement": return state - 1;
-        default: return state;
-    }
-}
+const counter = (state = 0, action: { type: string }) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 export default counter;
